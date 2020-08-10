9948e7ac2cadb3855026350d5d3ccf08db0259f1
szilex <szilex@op.pl>
2020-08-10 09:02:28 +0200

    Move mail sending to separate thread

--- mentoring/src/main/java/com/euvic/mentoring/config/ApplicationConfiguration.java
+++ mentoring/src/main/java/com/euvic/mentoring/config/ApplicationConfiguration.java
@@ -3,10 +3,15 @@ package com.euvic.mentoring.config;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import java.util.concurrent.Executor;
+
 @Configuration
+@EnableAsync
 public class ApplicationConfiguration {
 
     @Bean
@@ -17,5 +22,16 @@ public class ApplicationConfiguration {
     @Bean
     public ModelMapper modelMapper() {
         return new ModelMapper();
+    }
+
+    @Bean (name = "taskExecutor")
+    public Executor taskExecutor() {
+        final ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(2);
+        executor.setMaxPoolSize(2);
+        executor.setQueueCapacity(100);
+        executor.setThreadNamePrefix("MailThread-");
+        executor.initialize();
+        return executor;
     }
 }
--- mentoring/src/main/java/com/euvic/mentoring/service/MailService.java
+++ mentoring/src/main/java/com/euvic/mentoring/service/MailService.java
@@ -3,10 +3,12 @@ package com.euvic.mentoring.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
+import java.util.concurrent.CompletableFuture;
 
 @Service
 public class MailService implements IMailService {
@@ -19,12 +21,14 @@ public class MailService implements IMailService {
     }
 
     @Override
+    @Async
     public void sendMail(String to, String subject, String text, boolean isHtmlContent) throws MessagingException {
         MimeMessage mimeMessage = javaMailSender.createMimeMessage();
         MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage, true);
         mimeMessageHelper.setTo(to);
         mimeMessageHelper.setSubject(subject);
         mimeMessageHelper.setText(text, isHtmlContent);
-        javaMailSender.send(mimeMessage);
+        //javaMailSender.send(mimeMessage);
+        CompletableFuture.runAsync(() -> javaMailSender.send(mimeMessage));
     }
 }
